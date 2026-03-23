427d39316ad87013bf85fcb0699e9c813bf8a3d9
Ashok Kumar <ashokgupta2678@gmail.com>
2026-03-23 17:52:49 +0530

    refactored problem 6 for better readiblity
    Signed-off-by: Ashok Kumar <ashokgupta2678@gmail.com>

--- p6.java
+++ p6.java
@@ -68,14 +68,7 @@ public class p6 {
                 // so now if you see this in the row we have square like for 2nd row we got 4 then next row start with 5
                 // similarly in 4 is even row then it is 16 and 5 is odd row so it become 4row square + 1
                 // similar patter in the col but in opposite direction
-                long ans;
-                // so first check which is greater
-                if(n > m){
-                    // so it means that row number is major here so passing it to the row here
-                     ans = Row(n , m);
-                }else{
-                    ans = Column(m , n);
-                }
+                long ans = getAns(n , m);
 
                 out.println(ans);
             }
@@ -88,7 +81,15 @@ public class p6 {
 
 
     }
-     static long Column(long number , long position){
+     static  long getAns(long n , long m){
+         if(n > m){
+             // so it means that row number is major here so passing it to the row here
+             return  RowDominated(n , m);
+         }else{
+             return ColumnDominated(m , n);
+         }
+     }
+     static long ColumnDominated(long number , long position){
         // so now this is column dominated so we have to treat the col here
         long odd = number % 2 != 0 ? number : number-1;
         long sqr = (odd * odd); // got the square so now we have to induce the number
@@ -97,12 +98,12 @@ public class p6 {
             // so it is odd
             return sqr - (position-1);
         }else{
-            // so the number is odd
+            // so the number is even
             return sqr + position;
         }
 
     }
-    static long Row(long number , long position){
+    static long RowDominated(long number , long position){
         // so now this is Row dominated so we have to treat the row here
         long even = number % 2 == 0 ? number : number-1;
         long sqr = (even * even); // got the square so now we have to induce the number
