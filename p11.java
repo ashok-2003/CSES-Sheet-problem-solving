887549eddafe6abddfe7ecf1f297a6080a1d52c6
Ashok Kumar <ashokgupta2678@gmail.com>
2026-05-04 02:31:18 +0530

    Solved question 11 with optimized solution
    Signed-off-by: Ashok Kumar <ashokgupta2678@gmail.com>

--- p11.java
+++ p11.java
@@ -1,16 +1,72 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class p11 {
-    public static void main(String[] args){
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        while(n-- > 0){
-            long a = in.nextLong();
-            long b = in.nextLong();
-            System.out.println(isPossible(a, b));
+    static class FastReader{
+        BufferedReader br;
+        StringTokenizer st;
+        public FastReader(){
+            br=new BufferedReader(new InputStreamReader(System.in));
         }
+        String next(){
+            while(st==null || !st.hasMoreTokens()){
+                try {
+                    st=new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+        int nextInt(){
+            return Integer.parseInt(next());
+        }
+        long nextLong(){
+            return Long.parseLong(next());
+        }
+        double nextDouble(){
+            return Double.parseDouble(next());
+        }
+        String nextLine(){
+            String str="";
+            try {
+                str=br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
 
-        in.close(); // closing the input to avoid the leak;
+    public static void main(String[] args){
+        try {
+            p11.FastReader in = new p11.FastReader();
+            int n = in.nextInt();
+            while (n-- > 0) {
+                long a = in.nextLong();
+                long b = in.nextLong();
+//            System.out.println(isPossible(a, b));  // Brute force solution
+
+                // so in each step we remove exactly 3 coins.
+                // so bare minimum if it's a solution, then it should be
+                // divided by 3, but now there can be a case where a = 9 and b = 0.
+                // so that's why one pile cannot be much larger than others.
+                // so the max solution is when we are removing 2 from one pile and 1 from another pile like 6, 3.
+                // so a should not be greater than 2 * b
+
+                if ((a + b) % 3 == 0 && (Math.max(a, b) <= 2 * Math.min(a, b))) {
+                    System.out.println("YES");
+                } else {
+                    System.out.println("NO");
+                }
+
+            }
+
+
+        }catch (Exception e) {
+            System.out.println(e);
+            return;
+        }
     }
     private static String isPossible(long a , long b) {
         boolean ans = solution(a,b);
